cb69cdb3deeee5a6bcffb372cc2415b94073f89e
tosicky <tosingeeboy@yahoo.com>
2021-11-27 22:28:54 -0400

    Update deploy_image.sh

--- deploy_image.sh
+++ deploy_image.sh
@@ -5,4 +5,4 @@ sudo amazon-linux-extras install docker
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
 
-docker run tosicky/allhandsdev:cms-app-v1
+sudo docker run tosicky/allhandsdev:cms-app-v1
