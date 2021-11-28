d37e7ff8bd8796d81c35e7ed97ec1d91c5f36cfe
tosicky <tosingeeboy@yahoo.com>
2021-11-27 20:14:06 -0400

    Create deploy_image.sh
    deploy script

---
+++ deploy_image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+docker run tosicky/allhandsdev:cms-app-v1
